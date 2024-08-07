bc2748a4b5e55b32704ec3f11a2124ecb1a1782e
xiongqiang <469712814@qq.com>
2024-08-07 16:47:12 +0800

    ES6 practice update

--- 【html+css】/代码/0_test_code/16_ES6.js
+++ 【html+css】/代码/0_test_code/16_ES6.js
@@ -1,17 +1,22 @@
-let s = 'Hello'
+// let s = 'Hello'
 
-for (let i = 0; i < 5; i++) {
-  console.log(s[i])
-  console.log(i)
+// for (let i = 0; i < 5; i++) {
+//   console.log(s[i])
+//   console.log(i)
+// }
 
-}
-
+//随机函数封装
 function getRandom (N, M) {
   return Math.floor(Math.random() * (M - N + 1)) + N
 }
 
-a = 2
-console.log(global.a)
+// 判断数组和对象
+function isObject (value) {
+  return value !== null && typeof value === 'object' && !Array.isArray(value)
+}
+
+// a = 2
+// console.log(global.a)
 
 console.log('...............')
 let [foo, bar] = [1]
@@ -32,17 +37,57 @@ const map = new Map()
 map.set('first', 'hello')
 map.set('second', 'world')
 
-for (let [key, value] of map) {
-  console.log(key + ' is ' + value)
-}
+// for (let [key, value] of map) {
+//   console.log(key + ' is ' + value)
+// }
 
 let greeting = `\`Yo\` World!`
 console.log(greeting)
 
 s = 'Hello world!'
-// s.startsWith()
+// console.log(s.startsWith('H'))
 // s.endsWith()
 // s.includes()
 
 const trimString = '   helloss'
 console.log(trimString.trimStart())
+
+// rest参数: 使用...变量名，这样就不需要使用arguments对象了
+function add (...values) {
+  let sum = 0
+  for (let i = 0; i < values.length; i++) {
+    sum += values[i]
+    console.log(values[i])
+  }
+  return sum
+}
+
+let sum = add(1, 2, 3, 4)
+console.log(sum)
+// arguments 变量写法 和rest 参数写法
+//arguments参数写法
+function sortNumber () {
+  return Array.from(arguments).sort()
+}
+
+let s1 = sortNumber(4, 1, 3, 4, 5, 6, 7, -1)
+console.log(s1)
+
+// rest 参数写法
+
+const sortNumber3 = (...values) => values.sort()
+console.log(sortNumber3(1, 2, 3, 4, 5555, 1, 2, 3))
+
+function push (arr, ...items) {
+  items.forEach((e) => {
+    arr.push(e)
+  })
+}
+
+let a = [2, 3]
+push(a, 1, 23, 2, 2, 1, 1)
+console.log(a)
+
+const f = function () {}
+
+console.log(f.name)
--- 【html+css】/代码/0_test_code/17_regExp.js
+++ 【html+css】/代码/0_test_code/17_regExp.js
@@ -2,15 +2,20 @@
 const REG1 = /\d+(?=%)/
 // 后行断言  匹配$100
 const REG2 = /(?<=$)d+/
+
 // 日期时间解析
 const REG_DAT = /(?<year>\d{4})-(?<mounth>\d{2})-(?<day>\d{2})/
 const matchObj = REG_DAT.exec('1995-05-17')
 for (let i = 0; i < matchObj.length; i++) {
   console.log(matchObj[i])
 }
+//使用$<组名>引用具名组
+const date1 = '2004-09-02'
+const REG3 = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/
+const res1 = date1.replace(REG3, '$<day>/$<month>/$<year>')
 
 // 判断是否为数据
-console.log(typeof matchObj)
+// console.log(typeof matchObj)
 // console.log(Array.isArray(matchObj))
 // console.log(matchObj instanceof Array)
 // console.log(Object.prototype.toString.call(matchObj))
@@ -21,4 +26,20 @@ let str = 'test1test2test3'
 let arr1 = Array.from(str.matchAll(/t(e)(st(\d?))/g))
 // 方法二
 // let arr2 = [...str.matchAll(/t(e)(st(\d?))/g)]
-console.log(arr1)
+// console.log(arr1)
+//
+// // setInterval(函数，间隔时间)
+// let m = setInterval(function () {
+//   console.log('一秒执行一次')
+// }, 1000)
+// let n = setInterval(() => {console.log(new Date())}, 1000)
+
+const text = 'zabbcdef'
+const re = /ab/d
+const result = re.exec(text)
+
+console.log(result)
+console.log(result.index) // 1
+console.log(result.indices) // [ [1, 3] ]
+console.log(typeof 123n) // [ [1, 3] ]
+
